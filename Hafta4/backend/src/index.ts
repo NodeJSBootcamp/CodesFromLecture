097c94cbfb5f3f8475b7b0d8eb35dce53a67b435
Huseyin Bilen <huseyinxbilen@gmail.com>
2023-09-16 00:13:00 +0300

    Cors Added

--- Hafta4/backend/src/index.ts
+++ Hafta4/backend/src/index.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response, NextFunction } from "express"
+var cors = require('cors')
 import userRouter from "./router/userrouter"
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/user",userRouter)
 
